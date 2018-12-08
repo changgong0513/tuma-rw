9f6df9ec9f2549ace9a09c4eec2dc18d11d57192
zhanglei <86521074@qq.com>
2018-12-09 05:26:22 +0800

    完成小程序日历插件的添加

--- miniprogram/pages/sign_in/sign_in.js
+++ miniprogram/pages/sign_in/sign_in.js
@@ -5,7 +5,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    demo6_days_style: [],
+    year: new Date().getFullYear(),      // 年份
+    month: new Date().getMonth() + 1,    // 月份
+    day: new Date().getDate(),
   },
 
   /**
@@ -62,5 +65,51 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  dayClick(event) {
+    let {day} = event.detail;
+    console.log("当前选择的日期：" + event.detail.year + "-" + event.detail.month +  "-" + day);
+
+    const days_count = new Date(this.data.year, this.data.month, 0).getDate();
+    console.log("当前选择的月总天数：" + days_count);
+
+    let demo6_days_style = new Array;
+
+    // 设置不同日期的背景颜色
+    /*
+    for (let i = 1; i <= days_count; i++) {
+        const date = new Date(this.data.year, this.data.month - 1, i);
+        if (i == 12) {
+            demo6_days_style.push({
+                month: 'current', day: i, color: 'white', background: '#b49eeb'
+            });
+        } else if (i == 17) {
+            demo6_days_style.push({
+                month: 'current', day: i, color: 'white', background: '#f5a8f0'
+            });
+        } else if (i == 21) {
+            demo6_days_style.push({
+                month: 'current', day: i, color: 'white', background: '#aad4f5'
+            });
+        } else if (i == 25) {
+            demo6_days_style.push({
+                month: 'current', day: i, color: 'white', background: '#84e7d0'
+            });
+        } else {
+            demo6_days_style.push({
+                month: 'current', day: i, color: 'black'
+            });
+        }
+    }
+    */
+
+    demo6_days_style.push({
+      month: 'current', day: day, color: 'white', background: '#84e7d0'
+    });
+
+    this.setData({
+        demo6_days_style
+    });
   }
 })
